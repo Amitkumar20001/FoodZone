5d374663d147782c92e06e55af34f4c93f34bb34
Amit Kumar <ui19co07@iiitsurat.ac.in>
2022-05-27 23:26:42 +0530

    Add files via upload

---
+++ Food_zone/src/screens/OrderPlacedScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function OrderPlacedScreen() {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Awesome</h1>
+    <p class="lead">You have successfully placed your order</p>
+  </div>
+</div>
+  )
+}
